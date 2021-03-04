817c51c8b0e96f03b25a9656bda5b0a6be88a8d4
YTcd <your_email@youremail.com>
2021-03-04 12:06:19 +0900

    20.03.04 출근 전

---
+++ hello class/Assets/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    public string name;
+    public string sound;
+
+    public void PlaySound()
+    {
+      Debug.Log(name + ":" + sound);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
