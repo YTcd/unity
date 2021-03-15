5aefa9ecac7e75283d670f5c22b0bc17a6afa9f7
YTcd <your_email@youremail.com>
2021-03-15 12:14:04 +0900

    dodge 인터페이스 전

---
+++ dodge/Assets/BulletSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate=0;        // 생성 주기
+    private float timeAfterSpawn; // 최근 생성후 지난 시간
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn=0;
+        spawnRate = Random.Range(spawnRateMin,spawnRateMax);
+
+        target = FindObjectOfType<PlayerController>().transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if(timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet
+                = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin,spawnRateMax);
+
+        }
+    }
+}
