49a25d4be043b4e5de185dc0dfc1b41ba4d01503
YTcd <lovemytae@naver.com>
2021-05-26 23:35:42 +0900

    아 울고싶다

--- Dang'sfirst/Assets/Scripts/CharacterMove.cs
+++ Dang'sfirst/Assets/Scripts/CharacterMove.cs
@@ -9,7 +9,7 @@ public class CharacterMove : MonoBehaviour
     public Transform cameraRotate;
     float mh;   //캠 회전축
     float mv;
-    public float speed = 200f;
+    public float speed = 500f;
     private int jumpCount = 0;
     public float rotateSpeed = 200f;
     public float jumpForce = 500f;
@@ -36,9 +36,10 @@ public class CharacterMove : MonoBehaviour
         {
             playerRigidbody.velocity = Vector3.zero;
             jumpCount++;
-            playerRigidbody.AddForce(new Vector3(0f, jumpForce * Time.deltaTime, 0f));
+            playerRigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
+
     void Move()
     {
         float xInput = Input.GetAxis("Horizontal");
@@ -47,17 +48,16 @@ public class CharacterMove : MonoBehaviour
         float xSpeed = xInput * speed;
         float zSpeed = zInput * speed;
 
-        Vector3 newVelocity = new Vector3(xSpeed, 0f , zSpeed);
-        //Y좌표로도 움직일 수도 있는 상태여서 이동시 위아래로는 영향이 가지 않도록 0으로 초기화
-
-        Vector3 grav = new Vector3(0, -9.8f, 0);
-        newVelocity = transform.forward * newVelocity.z + transform.right * newVelocity.x + transform.up * grav.y* Time.deltaTime;
+        Vector3 newVelocity;
+        newVelocity = transform.right * xSpeed + transform.forward * zSpeed;
+        
         //transform.forward는 월드 좌표 기준 오브젝트의 회전 값을 반영한 normalized된 값을 반환합니다.
         //현재의 forward와 입력받은 값(방향)을 곱하여, 캐릭터를 기준(로컬 좌표)으로 캐릭터를 이동합니다.
 
 
-        playerRigidbody.velocity = newVelocity * Time.deltaTime * 50f;
+        playerRigidbody.velocity = newVelocity * Time.deltaTime;
     }
+
     void CamRotate()
     {
         float h = Input.GetAxis("Mouse X"); //마우스의 X축 움직임 감지
@@ -80,13 +80,13 @@ public class CharacterMove : MonoBehaviour
 
         cameraRotate.transform.eulerAngles = new Vector3(-mv, mh, 0);    //오일러 각으로 변환해서 캠 회전
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         isGrounded = true;
         jumpCount = 0;
         // 바닥에 닿았음을 감지하는 처리
     }
-
 
     private void OnCollisionExit(Collision collision)
     {
---
+++ Dang'sfirst/Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+
+    
+{
+    private Rigidbody playerRigidbody;
+
+    public float jumpForce = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //playerRigidbody.velocity = Vector3.zero;
+                playerRigidbody.AddForce(Vector3.up * jumpForce);
+            }
+    }
+}
